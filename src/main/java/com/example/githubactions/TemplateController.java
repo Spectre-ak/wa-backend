1df1654f784844444de11455282f7a46a5c6c1d6
Akash Upadhyay <62694340+Spectre-ak@users.noreply.github.com>
2021-05-08 13:03:32 +0530

    Update TemplateController.java

--- src/main/java/com/example/githubactions/TemplateController.java
+++ src/main/java/com/example/githubactions/TemplateController.java
@@ -1,4 +1,5 @@
 package com.example.githubactions;
+import java.io.File;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +9,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TemplateController {
 
-	@RequestMapping("/")
+		@RequestMapping("/")
 	public String index() {
+		if(checkCred() || checkToken()) {
+			return "downloaded";
+		}
+		
 		return "application satarted";
+	}
+	
+
+	boolean checkCred() {
+		File f=new File("credentialsSheets.json");
+		if(f.exists()) {
+			return true;
+		}
+		else return false;
+	}
+	
+	boolean checkToken() {
+		File f=new File("tokens");
+		if(f.exists()) {
+			return true;
+		}
+		else return false;
 	}
 }
